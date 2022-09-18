80a49a0155b4b6cb1656a48e285f39cfc4d2d1e9
Abdulazeez Sodiq <70654281+Abdulzeez403@users.noreply.github.com>
2022-09-17 17:49:53 -0700

    New features

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 
-// const baseURL =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid= ${api}`;
 
-// let api = '9834506ddad2b1c50b68dac71bf3fa3e';
 
 
 const cities = document.querySelector('input');
@@ -11,31 +9,32 @@ const countries = document.querySelector('.country');
 const origin = document.querySelector('.name');
 const img = document.querySelector('.weather_img');
 const text = document.querySelector('.city');
+const  weather_detail = document.querySelector('.weather_detail')
 
 cities.addEventListener('keypress',()=>{
   const input = cities.value;
-  const city = "Offa"
+  weather_detail.style.display= 'inline-block'
   const key = '2c98c921e50747cfa41185246221109';
   const base = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${input}&aqi=no`;
-   
-  
   fetch(base)
   .then( res => res.json())
   .then(data =>{
   console.log(data);
   // const lagtitude = data.location.lon;
   // const  longtitude = data.location.lat;
-   const country= data.location.country;
+   const country = data.location.country;
    const city = data.location.name;
    const origins = data.location.region;
    const imgs = data.current.condition.icon; 
    const texts = data.current.condition.text;
-  
+   const temp = data.current.temp_c;
+  //  const err = data.err.message;
+  //  err ? alert("err has ocucured"): alert('successfully');
   
   //  lat.textContent = `lag ${lagtitude} `;
-  //  long.textContent = `long ${longtitude}`;
+   long.textContent = ` ${temp}`;
    countries.textContent = `${country}`;
-   origin.textContent = ` ${city} / ${origins}`;
+   origin.textContent = ` ${city} | ${origins}`;
    img.src = `${imgs} `;
    text.textContent = texts;
   });
